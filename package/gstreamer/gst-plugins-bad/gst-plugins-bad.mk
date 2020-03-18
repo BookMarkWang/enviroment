145cc82289f1425e4de323b468665fa5cdcc7c5f
wangyang <ohwangyang@163.com>
2020-03-18 16:36:56 +0800

    add wayland to debug waylandsink/and user defined wayland window

--- package/gstreamer/gst-plugins-bad/gst-plugins-bad.mk
+++ package/gstreamer/gst-plugins-bad/gst-plugins-bad.mk
@@ -2,7 +2,7 @@ GST_PLUGINS_BAD_VERSION = 1.14.4
 GST_PLUGINS_BAD_SOURCE = gst-plugins-bad-$(GST_PLUGINS_BASE_VERSION).tar.xz
 GST_PLUGINS_BAD_SITE = https://gstreamer.freedesktop.org/src/gst-plugins-bad
 
-GST_PLUGINS_BAD_DEPENDENCIES=gstreamer gst-plugins-base
+GST_PLUGINS_BAD_DEPENDENCIES=gstreamer gst-plugins-base wayland
 
 GST_PLUGINS_BAD_CONF_OPTS=
 
